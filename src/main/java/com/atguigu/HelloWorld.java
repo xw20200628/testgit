03b7ed3187635f1f0c9998a44b89e0a7b7b419e1
xw <15268201404@163.com>
2021-03-28 17:10:04 +0800

    任务完成

--- src/main/java/com/atguigu/HelloWorld.java
+++ src/main/java/com/atguigu/HelloWorld.java
@@ -3,7 +3,7 @@ package com.atguigu;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("world");
-		System.out.println("hello");
+		
+		System.out.println("hello,world");
 	}
 }
